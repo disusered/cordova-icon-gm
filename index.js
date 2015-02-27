d6453cc933207532d34adb361f4dbb9556c7ff8a
Carlos Rosquillas <crosquillas@gmail.com>
2015-02-27 00:07:02 -0600

    Remove xxhdpi

--- index.js
+++ index.js
@@ -46,8 +46,7 @@ var getPlatforms = function (projectName) {
             { name : 'drawable-hdpi/icon.png',  size : 72 },
             { name : 'drawable-ldpi/icon.png',  size : 36 },
             { name : 'drawable-mdpi/icon.png',  size : 48 },
-            { name : 'drawable-xhdpi/icon.png', size : 96 },
-            { name : 'drawable-xxhdpi/icon.png', size : 144 },
+            { name : 'drawable-xhdpi/icon.png', size : 96 }
         ]
     });
     // TODO: add all platforms
